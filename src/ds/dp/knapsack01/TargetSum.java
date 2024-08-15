531d6eb1ffac12ca21d32bf441beacba28476119
NilimeshHait <saan.nilimesh044@gmail.com>
2024-08-16 00:22:09 +0530

    Target Sum

---
+++ src/ds/dp/knapsack01/TargetSum.java
@@ -0,0 +1,16 @@
+package ds.dp.knapsack01;
+
+/**
+ * 0/1 Knapsack problem:: Variant 6 (Target Sum)
+ * input : arr[1,1,2,3] , Sum =1
+ * Description : add +/- before each array value to get the expected Sum. Find the no of unique count
+ *                   by changing  sign(+/-) for the each array number.
+ * output : 3 ===> [+1,-1,-2,+3],[-1,+1,-2,+3],[+1,+1,+2,-3]
+ *
+ */
+
+public class TargetSum {
+    public static void main(String[] args) {
+        //Same as Variant 5 Count The Number Of Subset For a Given Difference
+    }
+}
